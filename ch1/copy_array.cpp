485b2829f225301f465e41ba9d23d5c02c1bade6
ChadMcIntire <chadmcintirecloud@gmail.com>
2020-08-19 10:47:05 -0600

    A few examples from chapter 1

---
+++ ch1/copy_array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void copy_fct()
+{
+    int v1[10] = {0,1,3,4,4,5,6,7,8,9};
+    int v2[10];
+
+    for (auto i=0; i!=10; ++i)
+        v2[i]=v1[i];
+
+    for (auto x : v2)
+        cout << x << '\n';
+}
+
+int main()
+{
+    copy_fct();
+}
