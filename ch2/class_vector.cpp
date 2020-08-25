91713770ac6ff6164ed6c37cd8b858c41200d44b
ChadMcIntire <chadmcintirecloud@gmail.com>
2020-08-25 11:24:53 -0600

    added copy con/assign, destructor, move con/assign

--- ch2/class_vector.cpp
+++ ch2/class_vector.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #include "class_vector.h" // get Vector's interface
 
-Vector::Vector(int s)
+Vector::Vector(int s)    //establish invariant, acquire resources
 {
     if (s<0)
         throw length_error{"Vector constructor: negative size"};
@@ -19,9 +19,65 @@ double& Vector::operator[](int i)
     return elem[i];
 }
 
+const double& Vector::operator[](int i) const
+{
+    if (i<0 || size()<i)
+        throw out_of_range{"Vector::operator[]"};
+    return elem[i];
+}
+
 int Vector::size() const
 {
      return sz;
+}
+
+Vector::~Vector(){
+    delete[] elem;  // destructor 
+}
+
+Vector::Vector(const Vector& a) //copy constructor
+    :elem{new double[a.sz]},    //allocate space for elements
+    sz{a.sz}
+{
+    for (int i=0; i!=sz; ++i) //copy elements
+        elem[i] = a.elem[i];
+}
+
+Vector& Vector::operator=(const Vector& a) //copy assignment
+{
+    double* p = new double[a.sz];  //a pointer to an allocate memory to an array of doubles
+    for (int i=0; i!=a.sz; ++i){
+        p[i] = a.elem[i]; //assign to the space
+    }
+    delete[] elem; //delete old elements
+    elem = p; //assign new elements 
+    sz = a.sz; //set size
+    return *this; //points to the object for which the member function is called
+}
+
+//non-member function operator
+Vector operator+(const Vector& a, const Vector& b)
+{
+  if (a.size()!=b.size())
+  {
+        throw out_of_range{"operator+ vector a and b different size"};
+  }
+
+  Vector res(a.size());
+  for (int i=0; i!=a.size(); ++i)
+  {
+      res[i]=a[i]+b[i];
+  }
+  return res;
+}
+
+//move constructor
+Vector::Vector(Vector&& a) //&& means rvalue reference
+    :elem{a.elem}, //grab elements from a
+    sz{a.sz}
+{
+    a.elem = nullptr;
+    a.sz = 0;
 }
 
 double read_and_sum(int s)
--- ch2/class_vector.h
+++ ch2/class_vector.h
@@ -1,8 +1,22 @@
 class Vector {
 public:
     Vector(int s); //default constructor
+    ~Vector(); //destructor
+
+    //copy
+    Vector(const Vector& a); //copy constuctor 
+    Vector& operator=(const Vector& a); //copy assignment
+
+    // bracket operators
     double& operator[](int i); //access element: subscripting
+    const double& operator[](int i) const; //access element: subscripting
+
+    //move 
+    Vector(Vector&& a);  //move constructor 
+    Vector& operator=(Vector&& a); //move assignment
+
     int size() const;
+
 private:
     double* elem; //pointer to the element
     int sz;       //the number of elements
